a3eb8ea53662fc3ad6d87153d5592a3b5763b22c
metalhead0 <147883336+metalhead0@users.noreply.github.com>
2025-10-30 01:39:15 +0100

    Initial Commit

---
+++ wspa logowanie/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  wspa logowanie
+//
+//  Created by Максим Полюх on 30/10/2025.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
---
+++ wspa logowanie/DocumentViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DocumentViewController.swift
+//  wspa logowanie
+//
+//  Created by Максим Полюх on 30/10/2025.
+//
+
+import UIKit
+
+class DocumentViewController: UIDocumentViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
